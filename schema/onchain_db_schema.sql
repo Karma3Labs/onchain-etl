6e350ecf8942c4543c6ed4564a99cb622050b229
Vijay Mariadassou <vijay@karma3labs.com>
2024-02-18 16:18:31 -0800

    pgdump

--- schema/onchain_db_schema.sql
+++ schema/onchain_db_schema.sql
@@ -2,10 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 15.5 (Debian 15.5-1.pgdg120+1)
--- Dumped by pg_dump version 16.0
-
--- Started on 2024-02-08 12:23:39 PST
+-- Dumped from database version 15.6 (Debian 15.6-1.pgdg120+2)
+-- Dumped by pg_dump version 16.1
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -23,11 +21,55 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 220 (class 1259 OID 20437)
--- Name: blocks; Type: TABLE; Schema: public; Owner: postgres
+-- Name: base_addresses_with_token_standards; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.blocks (
+CREATE TABLE public.base_addresses_with_token_standards (
+    contract_address character varying(42) NOT NULL,
+    token_standard character varying NOT NULL
+);
+
+
+ALTER TABLE public.base_addresses_with_token_standards OWNER TO postgres;
+
+--
+-- Name: base_transactions_sums; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.base_transactions_sums (
+    from_address character varying(42) NOT NULL,
+    max_timestamp timestamp without time zone NOT NULL,
+    min_timestamp timestamp without time zone NOT NULL,
+    to_address character varying(42),
+    total_gas_paid bigint NOT NULL,
+    txn_value numeric NOT NULL,
+    txn_count numeric NOT NULL,
+    to_address_is_contract boolean NOT NULL,
+    to_address_is_erc20 boolean NOT NULL,
+    to_address_is_erc721 boolean NOT NULL
+);
+
+
+ALTER TABLE public.base_transactions_sums OWNER TO postgres;
+
+--
+-- Name: contracts; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.contracts (
+    address character varying(42) NOT NULL,
+    is_erc20 boolean NOT NULL,
+    is_erc721 boolean NOT NULL
+);
+
+
+ALTER TABLE public.contracts OWNER TO postgres;
+
+--
+-- Name: deprecated_blocks; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.deprecated_blocks (
     number bigint NOT NULL,
     hash character varying(255) NOT NULL,
     parent_hash character varying(255),
@@ -52,24 +94,52 @@ CREATE TABLE public.blocks (
 );
 
 
-ALTER TABLE public.blocks OWNER TO postgres;
+ALTER TABLE public.deprecated_blocks OWNER TO postgres;
 
 --
--- TOC entry 224 (class 1259 OID 36989)
--- Name: contracts; Type: TABLE; Schema: public; Owner: postgres
+-- Name: deprecated_transaction_sums_old; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.contracts (
-    address character varying(42) NOT NULL,
-    is_erc20 boolean NOT NULL,
-    is_erc721 boolean NOT NULL
+CREATE TABLE public.deprecated_transaction_sums_old (
+    from_address character varying(42) NOT NULL,
+    to_address character varying(42),
+    gas_price bigint NOT NULL,
+    gas_used bigint NOT NULL,
+    txn_value numeric NOT NULL,
+    to_address_is_contract boolean NOT NULL,
+    to_address_is_erc20 boolean NOT NULL,
+    to_address_is_erc721 boolean NOT NULL
 );
 
 
-ALTER TABLE public.contracts OWNER TO postgres;
+ALTER TABLE public.deprecated_transaction_sums_old OWNER TO postgres;
 
 --
--- TOC entry 225 (class 1259 OID 45187)
+-- Name: deprecated_transactions; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.deprecated_transactions (
+    hash character varying(255) NOT NULL,
+    nonce bigint,
+    block_hash character varying(255),
+    block_number bigint,
+    transaction_index integer,
+    from_address character varying(255),
+    to_address character varying(255),
+    value numeric,
+    gas bigint,
+    gas_price bigint,
+    input text,
+    block_timestamp bigint,
+    max_fee_per_gas bigint,
+    max_priority_fee_per_gas bigint,
+    transaction_type integer
+);
+
+
+ALTER TABLE public.deprecated_transactions OWNER TO postgres;
+
+--
 -- Name: dune_labels; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -88,7 +158,6 @@ CREATE TABLE public.dune_labels (
 ALTER TABLE public.dune_labels OWNER TO postgres;
 
 --
--- TOC entry 222 (class 1259 OID 20591)
 -- Name: ens_addresses; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -103,7 +172,52 @@ CREATE TABLE public.ens_addresses (
 ALTER TABLE public.ens_addresses OWNER TO postgres;
 
 --
--- TOC entry 219 (class 1259 OID 16405)
+-- Name: eth_base_op_non_eoa_address_labels; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.eth_base_op_non_eoa_address_labels (
+    address character varying(42) NOT NULL,
+    category character varying NOT NULL,
+    label_type character varying NOT NULL,
+    model_name character varying NOT NULL
+);
+
+
+ALTER TABLE public.eth_base_op_non_eoa_address_labels OWNER TO postgres;
+
+--
+-- Name: eth_transactions_sums; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.eth_transactions_sums (
+    from_address character varying(42) NOT NULL,
+    to_address character varying(42) NOT NULL,
+    min_timestamp timestamp without time zone NOT NULL,
+    max_timestamp timestamp without time zone NOT NULL,
+    total_gas_value numeric NOT NULL,
+    total_txn_value numeric NOT NULL,
+    txn_count integer NOT NULL,
+    to_address_is_contract boolean NOT NULL,
+    to_address_is_erc20 boolean NOT NULL,
+    to_address_is_erc721 boolean NOT NULL
+);
+
+
+ALTER TABLE public.eth_transactions_sums OWNER TO postgres;
+
+--
+-- Name: ethereum_addresses_with_token_standards; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.ethereum_addresses_with_token_standards (
+    contract_address character varying(42) NOT NULL,
+    token_standard character varying NOT NULL
+);
+
+
+ALTER TABLE public.ethereum_addresses_with_token_standards OWNER TO postgres;
+
+--
 -- Name: labeled_addresses; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -119,7 +233,35 @@ CREATE TABLE public.labeled_addresses (
 ALTER TABLE public.labeled_addresses OWNER TO postgres;
 
 --
--- TOC entry 223 (class 1259 OID 28794)
+-- Name: optimism_addresses_with_token_standards; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.optimism_addresses_with_token_standards (
+    contract_address character varying(42) NOT NULL,
+    token_standard character varying NOT NULL
+);
+
+
+ALTER TABLE public.optimism_addresses_with_token_standards OWNER TO postgres;
+
+--
+-- Name: optimism_transaction_sums; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.optimism_transaction_sums (
+    from_address character varying(42) NOT NULL,
+    to_address character varying(42),
+    min_timestamp timestamp without time zone NOT NULL,
+    max_timestamp timestamp without time zone NOT NULL,
+    gas_price bigint NOT NULL,
+    gas_used bigint NOT NULL,
+    txn_value numeric NOT NULL
+);
+
+
+ALTER TABLE public.optimism_transaction_sums OWNER TO postgres;
+
+--
 -- Name: transaction_sums; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -140,42 +282,35 @@ CREATE TABLE public.transaction_sums (
 ALTER TABLE public.transaction_sums OWNER TO postgres;
 
 --
--- TOC entry 218 (class 1259 OID 16395)
--- Name: transactions; Type: TABLE; Schema: public; Owner: postgres
+-- Name: transaction_sums_with_count; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.transactions (
-    hash character varying(255) NOT NULL,
-    nonce bigint,
-    block_hash character varying(255),
-    block_number bigint,
-    transaction_index integer,
-    from_address character varying(255),
-    to_address character varying(255),
-    value numeric,
-    gas bigint,
-    gas_price bigint,
-    input text,
-    block_timestamp bigint,
-    max_fee_per_gas bigint,
-    max_priority_fee_per_gas bigint,
-    transaction_type integer
+CREATE TABLE public.transaction_sums_with_count (
+    from_address character varying(42) NOT NULL,
+    to_address character varying(42),
+    min_timestamp timestamp without time zone NOT NULL,
+    max_timestamp timestamp without time zone NOT NULL,
+    gas_price bigint NOT NULL,
+    gas_used bigint NOT NULL,
+    txn_value numeric NOT NULL,
+    txn_count numeric NOT NULL,
+    to_address_is_contract boolean NOT NULL,
+    to_address_is_erc20 boolean NOT NULL,
+    to_address_is_erc721 boolean NOT NULL
 );
 
 
-ALTER TABLE public.transactions OWNER TO postgres;
+ALTER TABLE public.transaction_sums_with_count OWNER TO postgres;
 
 --
--- TOC entry 3232 (class 2606 OID 20443)
--- Name: blocks blocks_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: deprecated_blocks blocks_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.blocks
+ALTER TABLE ONLY public.deprecated_blocks
     ADD CONSTRAINT blocks_pkey PRIMARY KEY (number);
 
 
 --
--- TOC entry 3242 (class 2606 OID 36993)
 -- Name: contracts contracts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -184,7 +319,6 @@ ALTER TABLE ONLY public.contracts
 
 
 --
--- TOC entry 3244 (class 2606 OID 45194)
 -- Name: dune_labels dune_labels_addresses_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -193,7 +327,6 @@ ALTER TABLE ONLY public.dune_labels
 
 
 --
--- TOC entry 3236 (class 2606 OID 20597)
 -- Name: ens_addresses ens_addresses_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -202,7 +335,6 @@ ALTER TABLE ONLY public.ens_addresses
 
 
 --
--- TOC entry 3230 (class 2606 OID 16411)
 -- Name: labeled_addresses labeled_addresses_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -211,24 +343,35 @@ ALTER TABLE ONLY public.labeled_addresses
 
 
 --
--- TOC entry 3227 (class 2606 OID 16401)
--- Name: transactions transactions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: deprecated_transactions transactions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.transactions
+ALTER TABLE ONLY public.deprecated_transactions
     ADD CONSTRAINT transactions_pkey PRIMARY KEY (hash);
 
 
 --
--- TOC entry 3223 (class 1259 OID 16404)
+-- Name: idx_base_transaction_sums_from_address_hash; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX idx_base_transaction_sums_from_address_hash ON public.base_transactions_sums USING hash (from_address);
+
+
+--
+-- Name: idx_base_transaction_sums_to_address_hash; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX idx_base_transaction_sums_to_address_hash ON public.base_transactions_sums USING hash (to_address);
+
+
+--
 -- Name: idx_block_number; Type: INDEX; Schema: public; Owner: postgres
 --
 
-CREATE INDEX idx_block_number ON public.transactions USING btree (block_number);
+CREATE INDEX idx_block_number ON public.deprecated_transactions USING btree (block_number);
 
 
 --
--- TOC entry 3237 (class 1259 OID 20601)
 -- Name: idx_ens_name; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -236,23 +379,34 @@ CREATE INDEX idx_ens_name ON public.ens_addresses USING btree (name);
 
 
 --
--- TOC entry 3224 (class 1259 OID 16402)
+-- Name: idx_eth_transactions_sums_from_address_hash; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX idx_eth_transactions_sums_from_address_hash ON public.eth_transactions_sums USING hash (from_address);
+
+
+--
+-- Name: idx_eth_transactions_sums_to_address_hash; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX idx_eth_transactions_sums_to_address_hash ON public.eth_transactions_sums USING hash (to_address);
+
+
+--
 -- Name: idx_from_address; Type: INDEX; Schema: public; Owner: postgres
 --
 
-CREATE INDEX idx_from_address ON public.transactions USING btree (from_address);
+CREATE INDEX idx_from_address ON public.deprecated_transactions USING btree (from_address);
 
 
 --
--- TOC entry 3233 (class 1259 OID 20444)
 -- Name: idx_hash; Type: INDEX; Schema: public; Owner: postgres
 --
 
-CREATE INDEX idx_hash ON public.blocks USING btree (hash);
+CREATE INDEX idx_hash ON public.deprecated_blocks USING btree (hash);
 
 
 --
--- TOC entry 3228 (class 1259 OID 36986)
 -- Name: idx_labeled_addresses_address; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -260,7 +414,6 @@ CREATE INDEX idx_labeled_addresses_address ON public.labeled_addresses USING btr
 
 
 --
--- TOC entry 3245 (class 1259 OID 45192)
 -- Name: idx_labels_addresses_hash_address; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -268,23 +421,20 @@ CREATE INDEX idx_labels_addresses_hash_address ON public.dune_labels USING hash 
 
 
 --
--- TOC entry 3234 (class 1259 OID 20445)
 -- Name: idx_miner; Type: INDEX; Schema: public; Owner: postgres
 --
 
-CREATE INDEX idx_miner ON public.blocks USING btree (miner);
+CREATE INDEX idx_miner ON public.deprecated_blocks USING btree (miner);
 
 
 --
--- TOC entry 3225 (class 1259 OID 16403)
 -- Name: idx_to_address; Type: INDEX; Schema: public; Owner: postgres
 --
 
-CREATE INDEX idx_to_address ON public.transactions USING btree (to_address);
+CREATE INDEX idx_to_address ON public.deprecated_transactions USING btree (to_address);
 
 
 --
--- TOC entry 3238 (class 1259 OID 36988)
 -- Name: idx_transaction_sums_from_address; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -292,7 +442,6 @@ CREATE INDEX idx_transaction_sums_from_address ON public.transaction_sums USING 
 
 
 --
--- TOC entry 3239 (class 1259 OID 37069)
 -- Name: idx_transaction_sums_from_address_hash; Type: INDEX; Schema: public; Owner: postgres
 --
 
@@ -300,14 +449,32 @@ CREATE INDEX idx_transaction_sums_from_address_hash ON public.transaction_sums U
 
 
 --
--- TOC entry 3240 (class 1259 OID 36987)
 -- Name: idx_transaction_sums_to_address; Type: INDEX; Schema: public; Owner: postgres
 --
 
 CREATE INDEX idx_transaction_sums_to_address ON public.transaction_sums USING btree (to_address);
 
 
--- Completed on 2024-02-08 12:23:58 PST
+--
+-- Name: idx_transaction_sums_with_count_from_address; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX idx_transaction_sums_with_count_from_address ON public.transaction_sums_with_count USING btree (from_address);
+
+
+--
+-- Name: idx_transaction_sums_with_count_from_address_hash; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX idx_transaction_sums_with_count_from_address_hash ON public.transaction_sums_with_count USING hash (from_address);
+
+
+--
+-- Name: idx_transaction_sums_with_count_to_address; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX idx_transaction_sums_with_count_to_address ON public.transaction_sums_with_count USING btree (to_address);
+
 
 --
 -- PostgreSQL database dump complete
